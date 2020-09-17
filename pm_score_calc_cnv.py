189a9b1f86c422478917078781bf85cca2006a4b
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2020-09-17 08:48:10 +0300

    fix formatting issues

--- pm_score_calc_cnv.py
+++ pm_score_calc_cnv.py
@@ -15,6 +15,7 @@ from datetime import datetime, timezone, timedelta
 import argparse
 import os
 
+
 class EnvDefault(argparse.Action):
     def __init__(self, envvar, required=True, default=None, **kwargs):
         if not default and envvar:
@@ -65,29 +66,29 @@ PRIORITY_SCORE = {
 class BugScore(object):
     def __init__(self, bz_id):
 
-        self.bug = bzapi.getbug(bz_id, include_fields=['_default','_custom','flags','external_bugs'])
+        self.bug = bzapi.getbug(bz_id, include_fields=['_default', '_custom', 'flags', 'external_bugs'])
 
     def calc_regression(self):
         score = 0
         if 'Regression' in self.bug.keywords:
             score = REGRESSION
-        #if score:
-            #print("Calculation debugs: Regression keyword: {}".format(score))
+        # if score:
+        # print("Calculation debugs: Regression keyword: {}".format(score))
         return score
 
     def calc_blocker(self):
         score = 0
         if self.bug.get_flag_status('blocker') is not None:
             score = BLOCKER
-        #if score:
-            #print("Calculation debugs: Blocker: {}".format(score))
+        # if score:
+        # print("Calculation debugs: Blocker: {}".format(score))
         return score
 
     def calc_automation_blocker(self):
         score = 0
         if 'AutomationBlocker' in self.bug.keywords:
             score = AUTOMATION_BLOCKER
-        #if score:
+        # if score:
         #    print("Calculation debugs: Automation Blocker: {}".format(score))
         return score
 
@@ -96,7 +97,7 @@ class BugScore(object):
         # print('INTERNAL: %s' % self.bug.cf_internal_whiteboard)
         if 'CEEBumpPMScore' in self.bug.cf_internal_whiteboard:
             score = CEEBumpPMScore
-        #if score:
+        # if score:
         #    print(
         #        "Calculation debugs: CEEBumpPMScore internal whiteboard: {}".format(score))
         return score
@@ -105,7 +106,7 @@ class BugScore(object):
         score = 0
         if self.bug.get_flag_status('cee_cir') == '+':
             score = CIR_FLAG
-        #if score:
+        # if score:
         #    print("Calculation debugs: cee_cir+ flag: {}".format(score))
         return score
 
@@ -118,17 +119,18 @@ class BugScore(object):
                 if "CEECIR" in x:
                     set_trace()
                     score = int(x.strip(",").strip("CEECIR_"))
-        #if score:
-        #    print("CalculatiThe only thing I have a question on is finding info about the on debugs: CEECIR_x in internal whiteboard: {}".format(score))
+        # if score:
+        #    print("The only thing I have a question on is finding info about the on debugs: "
+        #          "CEECIR_x in internal whiteboard: {}".format(score))
         return score
 
     def calc_severity(self):
-        #if self.bug.severity != "unspecified":
+        # if self.bug.severity != "unspecified":
         #    print("Calculation debugs: Severity score: {}".format(SEVERITY_SCORE[self.bug.severity]))
         return SEVERITY_SCORE[self.bug.severity]
 
     def calc_priority(self):
-        #if self.bug.priority != "unspecified":
+        # if self.bug.priority != "unspecified":
         #    print("Calculation debugs: Priority score: {}".format(PRIORITY_SCORE[self.bug.priority]))
         return PRIORITY_SCORE[self.bug.priority]
 
@@ -147,13 +149,13 @@ class BugScore(object):
                     ticket_totals += CUSTOMER_TICKET_OPEN
                     if NOTICKETS_KW in self.bug.cf_internal_whiteboard:
                         foo = self.bug.cf_internal_whiteboar
-                        foo = foo.replace(", "+NOTICKETS_KW, "").replace(NOTICKETS_KW, "")
+                        foo = foo.replace(", " + NOTICKETS_KW, "").replace(NOTICKETS_KW, "")
                         self.bug.cf_internal_whiteboard = foo
                         print("    NoCustomerTickets flag removed")
                         bzapi.update_bugs(self.bug.id, {
                             'cf_internal_whiteboard': self.bug.cf_internal_whiteboard,
                             'nomail': 1}
-                        )
+                                          )
         if ticket_open == 0 and ticket_count != 0:
             if NOTICKETS_KW not in self.bug.cf_internal_whiteboard:
                 if self.bug.cf_internal_whiteboard.strip() == "":
@@ -164,9 +166,9 @@ class BugScore(object):
                 bzapi.update_bugs(self.bug.id, {
                     'cf_internal_whiteboard': self.bug.cf_internal_whiteboard,
                     'nomail': 1}
-                )
+                                  )
         score = ticket_count * ticket_totals
-        #if score:
+        # if score:
         #    print("Calculation debugs: Tickets: {}".format(score))
         return score
 
@@ -191,19 +193,20 @@ class BugScore(object):
         else:
             print("    New score was not updated")
 
-#verify parameters
+
+# verify parameters
 # - must have specified... 
 #   - config file
 def verifyParameters():
     try:
-        #print (len(sys.argv))
+        # print (len(sys.argv))
         if len(sys.argv) <= 1:
-            #print("we are in trouble....")
+            # print("we are in trouble....")
             raise ValueError("invalid number of arguments...")
         else:
             pass
-            #print("we are on a good path....")
-            #raise Exception("Just testing...")
+            # print("we are on a good path....")
+            # raise Exception("Just testing...")
     except ValueError as e:
         print(str(e))
         print(getUsage())
@@ -213,11 +216,13 @@ def verifyParameters():
         print("Something else went wrong")
         raise
 
+
 def getUsage():
     return "usage " + __file__ + "  <BUGZILLA_API_KEY>"
 
+
 def utcformat(dt, timespec='milliseconds'):
-    #"""convert datetime to string in UTC format (YYYY-mm-ddTHH:MM:SS.mmmZ)"""
+    # """convert datetime to string in UTC format (YYYY-mm-ddTHH:MM:SS.mmmZ)"""
     iso_str = dt.astimezone(timezone.utc).isoformat('T', timespec)
     return iso_str.replace('+00:00', 'Z')
 
@@ -226,27 +231,31 @@ def fromutcformat(utc_str, tz=None):
     iso_str = utc_str.replace('Z', '+00:00')
     return datetime.fromisoformat(iso_str).astimezone(tz)
 
+
 def checkArguments():
     parser = argparse.ArgumentParser(description=__file__ + ' command line arguments')
     parser.add_argument('-k', '--key', required=True, type=str, action=EnvDefault, envvar='BZ_API_KEY',
                         help='The Bugzilla API key')
-    parser.add_argument('-p', '--time_delta_param', required=False, type=str, action=EnvDefault, envvar='TIME_DELTA_PARAM',
+    parser.add_argument('-p', '--time_delta_param', required=False, type=str, action=EnvDefault,
+                        envvar='TIME_DELTA_PARAM',
                         help='The time delta parameter: hours or days')
-    parser.add_argument('-v', '--time_delta_value', required=False, type=int, action=EnvDefault, envvar='TIME_DELTA_VALUE',
-                        help='The time delta value: number of [hours|days]. For hours use 1 to 23, and for days use 1 to 30.')
+    parser.add_argument('-v', '--time_delta_value', required=False, type=int, action=EnvDefault,
+                        envvar='TIME_DELTA_VALUE',
+                        help='The time delta value: number of [hours|days]. '
+                             'For hours use 1 to 23, and for days use 1 to 30.')
     args = parser.parse_args()
 
-    #check passed arguments
-    valid_time_delta_params = ['hours','days']
-    valid_hours = [1,23]
-    valid_days = [1,30]
+    # check passed arguments
+    valid_time_delta_params = ['hours', 'days']
+    valid_hours = [1, 23]
+    valid_days = [1, 30]
 
     errormessage = ""
 
-    if args.time_delta_param != None:
+    if args.time_delta_param is not None:
         if args.time_delta_param in valid_time_delta_params:
-            if args.time_delta_value == None:
-                    errormessage = "The time_delta_value must be specified"
+            if args.time_delta_value is None:
+                errormessage = "The time_delta_value must be specified"
             else:
                 if args.time_delta_param == "hours":
                     if args.time_delta_value < valid_hours[0] or args.time_delta_value > valid_hours[1]:
@@ -266,42 +275,42 @@ def checkArguments():
 
 
 if __name__ == "__main__":
-#    main()
+    #    main()
 
-#def main():
-#verify parameters
+    # def main():
+    # verify parameters
     try:
         args = checkArguments()
         in_api_key = args.key
 
-        #bzapi = RHBugzilla(url=URL, user=_user, password=_password)
+        # bzapi = RHBugzilla(url=URL, user=_user, password=_password)
 
-        bzapi = RHBugzilla(url=URL,api_key=in_api_key)
+        bzapi = RHBugzilla(url=URL, api_key=in_api_key)
 
         query = {
             'bug_status': [
                 'NEW', 'ASSIGNED', 'POST', 'MODIFIED', 'ON_QA', 'VERIFIED'
             ],
-            'product':'Container Native Virtualization (CNV)',
-            'include_fields':['_default','_custom','flags','external_bugs']
+            'product': 'Container Native Virtualization (CNV)',
+            'include_fields': ['_default', '_custom', 'flags', 'external_bugs']
         }
 
-        #check to see if the user specified a time delta for the query
+        # check to see if the user specified a time delta for the query
         lastchangetime = datetime.now()
-        if args.time_delta_param != None:
+        if args.time_delta_param is not None:
             if args.time_delta_param == "hours":
                 lastchangetime = lastchangetime - timedelta(hours=args.time_delta_value)
             else:
                 lastchangetime = lastchangetime - timedelta(days=args.time_delta_value)
-            query['last_change_time'] = utcformat(lastchangetime,timespec='seconds')
+            query['last_change_time'] = utcformat(lastchangetime, timespec='seconds')
 
         bugs = bzapi.query(query)
-        print ("Number of BZ to update: %s" % len(bugs))
+        print("Number of BZ to update: %s" % len(bugs))
         print("Bug ID, before, after")
         i = 1
         for bug in bugs:
-            #print(str(i) + "," + str(bug.id))
-            #print(bug._bug_fields)
+            # print(str(i) + "," + str(bug.id))
+            # print(bug._bug_fields)
             bz = BugScore(bug.id)
             print(str(i) + "," + str(bug.id) + "," + bug.cf_pm_score + "," + str(bz.calc_score()))
             bz.update()
