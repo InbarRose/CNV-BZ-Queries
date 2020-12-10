50e8633b6d5b1080a047cd41dfbcb8eef918bf93
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2020-12-10 11:45:38 +0200

    Refactor code for logging and testing
    added better support for testing
    refactored methods for robustness
    added logging
    improved flow
    improved logging

--- pm_score_calc_cnv.py
+++ pm_score_calc_cnv.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
-from __future__ import division, print_function
 
 import ssl
 import argparse
 import os
 import datetime
+import logging
+import collections
 
 # import bugzilla
 from bugzilla.rhbugzilla import RHBugzilla
 
-INCLUDE_FIELDS = ['_default', '_custom', 'flags', 'external_bugs']
+logger = logging.getLogger('bz_pm_score_updater')
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+TimeMinMax = collections.namedtuple('TimeMinMax', 'min max')
+
 # Constants
 URL = "bugzilla.redhat.com"
 NO_TICKETS_KW = "NoActiveCustomerTickets"
+REDHAT_COM_SUPPORT_CASES = 'https://access.redhat.com/support/cases'
 PRODUCT_NAME = 'Container Native Virtualization (CNV)'
 VALID_BUG_STATUS_LIST = [
     'NEW',
@@ -27,6 +31,15 @@ VALID_BUG_STATUS_LIST = [
     'ON_QA',
     'VERIFIED'
 ]
+INCLUDE_FIELDS = [
+    '_default',
+    '_custom',
+    'flags',
+    'external_bugs'
+]
+VALID_TIME_TYPES = ['hours', 'days']
+VALID_HOURS = TimeMinMax(1, 23)
+VALID_DAYS = TimeMinMax(1, 30)
 
 # Scores
 REGRESSION = 600
@@ -95,15 +108,22 @@ class BZBugScoreUpdater(object):
         return query
 
     def perform_bug_score_updates(self, last_change_time=None):
+        bugs = self._fetch_bugs(last_change_time)
+        self._update_bug_scores(bugs)
+
+    def _fetch_bugs(self, last_change_time):
         query = self.make_query(last_change_time)
+        logger.info(f"Querying BugZilla: {query}")
         bugs = self.query_bugs(query)
-        print(f"Number of BZ Bugs to update: {len(bugs)}")
-        print("Index, Bug ID, before, after")
+        return bugs
+
+    def _update_bug_scores(self, bugs):
+        logger.info(f"Number of BZ Bugs found in query: {len(bugs)}")
+        logger.info("Index: Score( before => after ) BZ_URL [<updated-status>]")
         for idx, bug in enumerate(bugs):
             bz_bs = self.get_bz_bug_scorer(bug.id)
-            bz_bs.calc_score()
-            print(f"{idx}, {bug.id}, {bug.cf_pm_score}, {bz_bs.score}")
-            bz_bs.update()
+            updated = bz_bs.update()  # implies bz_bs.calc_score()
+            logger.info(f"{idx:03}:\t( {bz_bs.old_score} => {bz_bs.new_score} ) {bz_bs.bug_url} [{updated}]")
 
 
 class BZBugScorer(object):
@@ -115,16 +135,42 @@ class BZBugScorer(object):
         self.bz_api = bz_api
         self.debug_mode = debug_mode
 
+    @property
+    def bug_url(self):
+        return f"https://{URL}/show_bug.cgi?id={self.bug.id}"
+
+    @property
+    def log_id(self):
+        return self.bug.id
+
+    @property
+    def old_score(self):
+        return int(self.bug.cf_pm_score)
+
+    @property
+    def new_score(self):
+        return int(self.score)
+
+    @property
+    def needs_update(self):
+        return bool(self.old_score != self.new_score)
+
+    def calculation_msg(self, msg):
+        self.debug_msg(f"{msg}")
+
     def debug_msg(self, msg):
         if self.debug_mode:
-            print(msg)
+            logger.debug(f"{self.log_id}: {msg}")
+
+    def info_msg(self, msg):
+        logger.info(f"{self.log_id}: {msg}")
 
     def calc_regression(self):
         score = 0
         if 'Regression' in self.bug.keywords:
             score = REGRESSION
         if score:
-            self.debug_msg(f"Calculation debugs: Regression keyword: {score}")
+            self.calculation_msg(f"Regression keyword: {score}")
         return score
 
     def calc_blocker(self):
@@ -132,7 +178,7 @@ class BZBugScorer(object):
         if self.bug.get_flag_status('blocker') is not None:
             score = BLOCKER
         if score:
-            self.debug_msg("Calculation debugs: Blocker: {}".format(score))
+            self.calculation_msg(f"Blocker: {score}")
         return score
 
     def calc_automation_blocker(self):
@@ -140,16 +186,16 @@ class BZBugScorer(object):
         if 'AutomationBlocker' in self.bug.keywords:
             score = AUTOMATION_BLOCKER
         if score:
-            self.debug_msg(f"Calculation debugs: Automation Blocker: {score}")
+            self.calculation_msg(f"Automation Blocker: {score}")
         return score
 
     def calc_cee(self):
         score = 0
-        self.debug_msg('INTERNAL: %s' % self.bug.cf_internal_whiteboard)
+        self.debug_msg(f'INTERNAL: {self.bug.cf_internal_whiteboard}')
         if 'CEEBumpPMScore' in self.bug.cf_internal_whiteboard:
             score = CEEBumpPMScore
         if score:
-            self.debug_msg(f"Calculation debugs: CEEBumpPMScore internal whiteboard: {score}")
+            self.calculation_msg(f"CEEBumpPMScore internal whiteboard: {score}")
         return score
 
     def calc_cir_flag(self):
@@ -157,55 +203,69 @@ class BZBugScorer(object):
         if self.bug.get_flag_status('cee_cir') == '+':
             score = CIR_FLAG
         if score:
-            self.debug_msg("Calculation debugs: cee_cir+ flag: {}".format(score))
+            self.calculation_msg(f"cee_cir+ flag: {score}")
         return score
 
     def calc_severity(self):
+        score = SEVERITY_SCORE[self.bug.severity]
         if self.bug.severity != "unspecified":
-            self.debug_msg(f"Calculation debugs: Severity score: {SEVERITY_SCORE[self.bug.severity]}")
-        return SEVERITY_SCORE[self.bug.severity]
+            self.calculation_msg(f"Severity score: {score}")
+        return score
 
     def calc_priority(self):
+        score = PRIORITY_SCORE[self.bug.priority]
         if self.bug.priority != "unspecified":
-            self.debug_msg(f"Calculation debugs: Priority score: {PRIORITY_SCORE[self.bug.priority]}")
-        return PRIORITY_SCORE[self.bug.priority]
+            self.calculation_msg(f"Priority score: {score}")
+        return score
+
+    @staticmethod
+    def item_is_support_case(item):
+        return bool(REDHAT_COM_SUPPORT_CASES in item['type']['full_url'])
 
     def calc_tickets(self):
-        ticket_totals = 0
-        ticket_count = 0
-        ticket_open = 0
-        for item in self.bug.external_bugs:
-            if 'https://access.redhat.com/support/cases' in item['type']['full_url']:
-                if item['ext_status'] == "Closed":
-                    ticket_count += 1
-                    ticket_totals += CUSTOMER_TICKET_CLOSED
-                elif "Waiting" in item['ext_status']:
-                    ticket_open += 1
-                    ticket_count += 1
-                    ticket_totals += CUSTOMER_TICKET_OPEN
-                    if NO_TICKETS_KW in self.bug.cf_internal_whiteboard:
-                        foo = self.bug.cf_internal_whiteboar
-                        foo = foo.replace(f", {NO_TICKETS_KW}", "").replace(NO_TICKETS_KW, "")
-                        self.bug.cf_internal_whiteboard = foo
-                        print("    NoCustomerTickets flag removed")
-                        # TODO: Why are we updating the bug here?
-                        self.bz_api.update_bugs(self.bug.id,
-                                                {'cf_internal_whiteboard': self.bug.cf_internal_whiteboard,
-                                                 'nomail': 1})
+        """
+        iterate over external bugs to find customer support case tickets
+        calculate score based on how many open or closed tickets we find
+
+        also will update the bug with a NoActiveCustomerTickets flag
+        on the bugs Internal Whiteboard to reflect the status tickets
+        """
+        ticket_totals, ticket_count, ticket_open = 0, 0, 0
+
+        for item in filter(self.item_is_support_case, self.bug.external_bugs):
+            if item['ext_status'] == "Closed":
+                ticket_count += 1
+                ticket_totals += CUSTOMER_TICKET_CLOSED
+            elif "Waiting" in item['ext_status']:
+                ticket_open += 1
+                ticket_count += 1
+                ticket_totals += CUSTOMER_TICKET_OPEN
+
+                if NO_TICKETS_KW in self.bug.cf_internal_whiteboard:
+                    self.remove_no_customer_ticket_flag()
+
         if ticket_open == 0 and ticket_count != 0:
             if NO_TICKETS_KW not in self.bug.cf_internal_whiteboard:
-                if self.bug.cf_internal_whiteboard.strip() == "":
-                    self.bug.cf_internal_whiteboard = NO_TICKETS_KW
-                else:
-                    self.bug.cf_internal_whiteboard = f"{self.bug.cf_internal_whiteboard}, {NO_TICKETS_KW}"
-                print("    NoCustomerTickets flag added")
-                # TODO: Why are we updating the bug here?
-                self.bz_api.update_bugs(self.bug.id,
-                                        {'cf_internal_whiteboard': self.bug.cf_internal_whiteboard, 'nomail': 1})
+                self.add_no_customer_ticket_flag()
+
         score = ticket_count * ticket_totals
         if score:
-            self.debug_msg(f"Calculation debugs: Tickets: {score}")
+            self.calculation_msg(f"Tickets: {score}")
         return score
+
+    def add_no_customer_ticket_flag(self):
+        if self.bug.cf_internal_whiteboard.strip() == "":
+            self.bug.cf_internal_whiteboard = NO_TICKETS_KW
+        else:
+            self.bug.cf_internal_whiteboard = f"{self.bug.cf_internal_whiteboard}, {NO_TICKETS_KW}"
+        self.bz_api.update_bugs(self.bug.id, {'cf_internal_whiteboard': self.bug.cf_internal_whiteboard, 'nomail': 1})
+        self.debug_msg("NoCustomerTickets flag added")
+
+    def remove_no_customer_ticket_flag(self):
+        new_text = self.bug.cf_internal_whiteboar.replace(f", {NO_TICKETS_KW}", "").replace(NO_TICKETS_KW, "")
+        self.bug.cf_internal_whiteboard = new_text
+        self.bz_api.update_bugs(self.bug.id, {'cf_internal_whiteboard': self.bug.cf_internal_whiteboard, 'nomail': 1})
+        self.debug_msg("NoCustomerTickets flag removed")
 
     def calc_score(self):
         """run all calculations and provide final score"""
@@ -222,16 +282,21 @@ class BZBugScorer(object):
         self.score = sum(score)
         return self.score
 
+    def _update(self):
+        self.bz_api.update_bugs(self.bug.id, {'cf_pm_score': self.score, 'nomail': 1})
+
     def update(self):
-        """perform score calculation and then update bug with new score"""
-        score = self.score or self.calc_score()  # do not calculate again if we already have the score
-        print(f"    New Score   = {score}")
-        self.debug_msg(f"    Old Score   = {int(self.bug.cf_pm_score)}")
-        if int(self.bug.cf_pm_score) != score:
-            self.bz_api.update_bugs(self.bug.id, {'cf_pm_score': score, 'nomail': 1})
-            print("    New score was updated")
+        """
+        perform score calculation and then update bug with new score if it is different than old score
+        returns True if score was updated, otherwise false.
+        """
+        if not self.score:
+            self.calc_score()  # do not calculate again if we already have the score
+        if self.needs_update:
+            self._update()
+            return True
         else:
-            print("    New score was not updated")
+            return False
 
 
 def utc_format(dt, timespec='milliseconds'):
@@ -240,45 +305,48 @@ def utc_format(dt, timespec='milliseconds'):
     return iso_str.replace('+00:00', 'Z')
 
 
+def get_utc_time_ago(time_type, time_value):
+    """
+    convert time delta into UTC formatted string representing a time in the past according to the specified delta
+    """
+    assert time_type in VALID_TIME_TYPES
+    if time_type == "hours":
+        delta = datetime.datetime.now() - datetime.timedelta(hours=time_value)
+    else:  # days
+        delta = datetime.datetime.now() - datetime.timedelta(days=time_value)
+    last_change_time = utc_format(delta, timespec='seconds')
+    return last_change_time
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description=f'{__file__} command line arguments')
     parser.add_argument('-k', '--key', required=True, type=str, action=EnvDefault,
-                        envvar='BZ_API_KEY',
+                        env_var='BZ_API_KEY',
                         help='The Bugzilla API key')
     parser.add_argument('-p', '--time_delta_param', required=False, type=str, action=EnvDefault,
-                        envvar='TIME_DELTA_PARAM',
-                        help='The time delta parameter: hours or days')
+                        env_var='TIME_DELTA_PARAM',
+                        help=f'The time delta parameter: {VALID_TIME_TYPES}')
     parser.add_argument('-v', '--time_delta_value', required=False, type=int, action=EnvDefault,
-                        envvar='TIME_DELTA_VALUE',
-                        help='The time delta value: number of [hours|days]. '
-                             'For hours use 1 to 23, and for days use 1 to 30.')
+                        env_var='TIME_DELTA_VALUE',
+                        help=f'The time delta value: number of {VALID_TIME_TYPES}. '
+                             f'For hours use {VALID_HOURS.min} to {VALID_HOURS.max}, '
+                             f'For days use {VALID_DAYS.min} to {VALID_DAYS.max}.')
     args = parser.parse_args()
 
-    # check time delta arguments are valid
-    valid_time_delta_params = ['hours', 'days']
-    valid_hours = [1, 23]
-    valid_days = [1, 30]
-
-    if args.time_delta_param is not None and args.time_delta_param not in valid_time_delta_params:
-        parser.error(f"Invalid time_delta_param. Valid values include: {valid_time_delta_params}")
+    if args.time_delta_param is not None and args.time_delta_param not in VALID_TIME_TYPES:
+        parser.error(f"Invalid time_delta_param. Valid values include: {VALID_TIME_TYPES}")
 
     if args.time_delta_param is not None and args.time_delta_value is None:
         parser.error("The time_delta_value must be specified")
 
-    if args.time_delta_param == "hours" and not valid_hours[0] <= args.time_delta_value <= valid_hours[1]:
-        parser.error(f"For hours use values from {valid_hours[0]} to {valid_hours[1]}")
+    if args.time_delta_param == "hours" and not VALID_HOURS.min <= args.time_delta_value <= VALID_HOURS.max:
+        parser.error(f"For hours use values from {VALID_HOURS.min} to {VALID_HOURS.max}")
 
-    if args.time_delta_param == "days" and not valid_days[0] <= args.time_delta_value <= valid_days[1]:
-        parser.error(f"For days use values from {valid_days[0]} to {valid_days[1]}")
+    if args.time_delta_param == "days" and not VALID_DAYS.min <= args.time_delta_value <= VALID_DAYS.max:
+        parser.error(f"For days use values from {VALID_DAYS.min} to {VALID_DAYS.max}")
 
-    # use time delta arguments
     if args.time_delta_param is not None and args.time_delta_value is not None:
-        # convert time delta into UTC formatted string representing a time in the past according to the specified delta
-        if args.time_delta_param == "hours":
-            delta = datetime.datetime.now() - datetime.timedelta(hours=args.time_delta_value)
-        else:
-            delta = datetime.datetime.now() - datetime.timedelta(days=args.time_delta_value)
-        last_change_time = utc_format(delta, timespec='seconds')
+        last_change_time = get_utc_time_ago(args.time_delta_param, args.time_delta_value)
     else:
         last_change_time = None
 
@@ -286,14 +354,16 @@ def parse_arguments():
 
 
 def main(key, last_change_time=None):
+    logger.info(f"Starting process: last_change_time={last_change_time}")
     bz_api = RHBugzilla(url=URL, api_key=key)
-
     bz_bsu = BZBugScoreUpdater(bz_api)
     bz_bsu.perform_bug_score_updates(last_change_time)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s: %(message)s")
     try:
         main(*parse_arguments())
     except Exception as exc:
-        print(f"exception in main... {exc}")
+        logger.exception(f"exception in main... {exc}", exc_info=True)
--- test_pm_score.py
+++ test_pm_score.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from pm_score_calc_cnv import RHBugzilla, BZBugScoreUpdater, URL
+from .pm_score_calc_cnv import RHBugzilla, BZBugScoreUpdater, BZBugScorer, URL, INCLUDE_FIELDS
 
 test_key = os.getenv('BZ_API_KEY')
 test_bug_id = os.getenv('BZ_TEST_ID')
@@ -9,17 +9,46 @@ test_bug_score = os.getenv('BZ_TEST_SCORE')
 
 class ScoreTester(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        assert test_key
+
     def test_get_bug_score(self):
-        self.assertIsNotNone(test_key)
         self.assertIsNotNone(test_bug_id)
         self.assertIsNotNone(test_bug_score)
-        expected_score = int(test_bug_score)
         bz_api = RHBugzilla(url=URL, api_key=test_key)
         bz_bsu = BZBugScoreUpdater(bz_api)
         bz_bs = bz_bsu.get_bz_bug_scorer(test_bug_id)
         score = bz_bs.calc_score()
         self.assertIsNotNone(score)
-        self.assertEquals(expected_score, score)
+        expected_score = int(test_bug_score)
+        self.assertEqual(expected_score, score)
+
+    def test_bz_url(self):
+        self.assertIsNotNone(test_bug_id)
+        bz_api = RHBugzilla(url=URL, api_key=test_key)
+        bz_bsu = BZBugScoreUpdater(bz_api)
+        bz_bs = bz_bsu.get_bz_bug_scorer(test_bug_id)
+        self.assertEqual(f"https://{URL}/show_bug.cgi?id={test_bug_id}", bz_bs.bug_url)
+
+    def test_mock_update_with_logging(self):
+        self.assertIsNotNone(test_bug_id)
+
+        with self.assertLogs('bz_pm_score_updater', level='INFO') as cm:
+            bz_api = RHBugzilla(url=URL, api_key=test_key)
+            bz_bsu = BZBugScoreUpdater(bz_api)
+            BZBugScorer._update = lambda: None  # mock the update process to do nothing
+            bug = bz_api.getbug(test_bug_id, include_fields=INCLUDE_FIELDS)
+            bz_bsu._update_bug_scores([bug])
+
+        self.assertEqual(
+            cm.output,
+            [
+                'INFO:bz_pm_score_updater:Number of BZ Bugs found in query: 1',
+                'INFO:bz_pm_score_updater:Index: Score( before => after ) BZ_URL [<updated-status>]',
+                f'INFO:bz_pm_score_updater:000:\t'
+                f'( {test_bug_score} => {test_bug_score} ) https://{URL}/show_bug.cgi?id={test_bug_id} [False]'
+            ])
 
 
 if __name__ == '__main__':
